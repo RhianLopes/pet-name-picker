89b4f7fc48674f7350002a97cf5bf1b6c657f67c
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2023-01-03 18:10:36 -0300

    feat: introducing result page

--- src/Pages/PetNameResult/index.js
+++ src/Pages/PetNameResult/index.js
@@ -1,6 +1,52 @@
-import React from 'react'
+/*eslint array-callback-return: 0*/
+/*eslint react-hooks/exhaustive-deps: 0*/
+
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useLocation } from 'react-router-dom'
 import './styles.scss'
 
 export function PetNameResult() {
-  return <div id='pet-name-result'>batata</div>
+  const location = useLocation()
+  const [result, setResult] = useState([])
+  const [filters] = useState(location.state.filters)
+  
+  useEffect(() => {
+    let prompt = '10 Sugestões de nomes brasileiros para animal de estimação '
+    filters.map((item) => {
+      prompt = prompt.concat(' ').concat(item)
+    })
+    axios.post('https://api.openai.com/v1/completions', 
+          {
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: 4000,
+            temperature: 1.0
+          },
+          { 
+            headers: {
+                // Authorization: "Bearer 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"
+            },
+          }
+        )
+        .then(response => setResult(response.data.choices[0].text.split(/\r?\n/).filter(entry => /\S/.test(entry))))
+        console.log(prompt)
+  }, [])
+
+  return (
+    <div id='pet-name-result'>
+      <h1 className='question-title'>Segue abaixo sugestões de nome para seu Pet:</h1>
+      <div className='result-card'>
+        <br/>
+        {result && result.map((item, index) => {
+          return (
+            <>
+              {(index + 1) + ' - ' + item.replace(/[^a-zA-Z]/g,'')} <br/>
+            </>
+          )
+        })}
+        <br/>
+      </div>
+    </div>
+  )
 }
--- src/Pages/PetQuestion/index.js
+++ src/Pages/PetQuestion/index.js
@@ -6,22 +6,25 @@ import './styles.scss'
 
 export function PetQuestion() {
   const [questionIndex, setQuestionIndex] = useState(0)
+  const [filter, setFilter] = useState([])
   const navigate = useNavigate()
 
-  function onSubmit() {
+  function onSubmit(request) {
     if (questionIndex === QUESTIONS.length - 1) {
-      navigate('/pet-name-picker/result')
+      navigate('/pet-name-picker/result', {state: { filters: [...filter, request] }})
     } else {
+      setFilter([...filter, request])
       setQuestionIndex(questionIndex + 1)
     }
+    console.log(filter)
   }
 
   return (
     <div id='pet-question'>
       <h1 className='question-title'>{QUESTIONS[questionIndex].question}</h1>
       <div className='container-options'>
-        <CardOption name={QUESTIONS[questionIndex].opt1} onClick={() => onSubmit()} />
-        <CardOption name={QUESTIONS[questionIndex].opt2} onClick={() => onSubmit()} />
+        <CardOption name={QUESTIONS[questionIndex].opt1} onClick={() => onSubmit(QUESTIONS[questionIndex].opt1)} />
+        <CardOption name={QUESTIONS[questionIndex].opt2} onClick={() => onSubmit(QUESTIONS[questionIndex].opt2)} />
       </div>
     </div>
   )
